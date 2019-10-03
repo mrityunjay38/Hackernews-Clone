62bf0a13f52306256b9717159906faca0c146149
MRITYUNJAY <mrityunjay.1@mountblue.io>
2019-10-03 16:49:51 +0530

    Adds table view for stories/counter/comments

--- src/App.js
+++ src/App.js
@@ -9,6 +9,24 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hacker News</h1>
       </header>
+      <section className="Story">
+        <table>
+          <tr>
+            <td>
+              <div className="Counter">
+            <button className="upvote"></button>
+            <span className="Count"></span>
+            <button className="downvote"></button>
+            </div>
+            </td>
+            <td><a href={**link}><h2>{**title}</h2></a></td>
+            <td>
+              <p>By {**user} | {**time} ago | {**comment count} <a href={comments}>comments</a>
+              </p>
+            </td>
+          </tr>
+        </table>
+      </section>
     </div>
   );
 }
