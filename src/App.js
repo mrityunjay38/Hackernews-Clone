26725472eada2bda65293544db84cb9cf15d8660
MRITYUNJAY <mrityunjay.1@mountblue.io>
2019-10-04 00:38:58 +0530

    Makes app.js cleaner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import logo from "./data/img/logo.gif";
 import "./data/css/App.css";
+import Stories from "./components/stories";
 
 function App() {
+  // console.log(stories);
   return (
     <div className="App">
       <header className="App-header">
@@ -11,20 +13,9 @@ function App() {
       </header>
       <section className="Story">
         <table>
-          <tr>
-            <td>
-              <div className="Counter">
-            <button className="upvote"></button>
-            <span className="Count"></span>
-            <button className="downvote"></button>
-            </div>
-            </td>
-            <td><a href={**link}><h2>{**title}</h2></a></td>
-            <td>
-              <p>By {**user} | {**time} ago | {**comment count} <a href={comments}>comments</a>
-              </p>
-            </td>
-          </tr>
+          <tbody>
+            <Stories />
+          </tbody>
         </table>
       </section>
     </div>
