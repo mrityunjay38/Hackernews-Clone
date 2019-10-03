43b2d7200c4d0f324627693594223fca5f00425e
MRITYUNJAY <mrityunjay.1@mountblue.io>
2019-10-04 01:11:26 +0530

    Adds upvote/downvote feature

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+
+    state = {
+    score: this.props.count
+    }
+
+    onUpvote = () => {
+        this.setState({
+        score: this.state.score + 1
+    })
+    }
+
+    onDownvote = () => {
+        this.setState({
+            score:this.state.score - 1
+        })
+    }
+
   render() {
     return (
       <td>
         <div className="Counter">
-          <button className="Upvote">&#8710;</button>
-          <span className="Count">{this.props.count}</span>
-          <button className="Downvote">&nabla;</button>
+          <button className="Upvote" onClick={this.onUpvote}>&#8710;</button>
+          <span className="Count">{this.state.score}</span>
+          <button className="Downvote" onClick={this.onDownvote}>&nabla;</button>
         </div>
       </td>
     );
