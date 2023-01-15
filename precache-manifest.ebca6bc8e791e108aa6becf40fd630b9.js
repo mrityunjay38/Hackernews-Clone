d06be13a973c86e656e1ea5af507743e29295178
Mrityunjay <mrityun.jay@indegene.com>
2023-01-15 12:29:16 +0530

    build [update]

---
+++ precache-manifest.ebca6bc8e791e108aa6becf40fd630b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402064afd41ad11eee8fb403a3f3736b",
+    "url": "/Hackernews-Clone/index.html"
+  },
+  {
+    "revision": "2e27e7ea003ad67ce81e",
+    "url": "/Hackernews-Clone/static/css/main.156bc7a8.chunk.css"
+  },
+  {
+    "revision": "bc98deec659539d26f1b",
+    "url": "/Hackernews-Clone/static/js/2.96390870.chunk.js"
+  },
+  {
+    "revision": "2e27e7ea003ad67ce81e",
+    "url": "/Hackernews-Clone/static/js/main.877ffdac.chunk.js"
+  },
+  {
+    "revision": "ee6f28635a2a511fc1e3",
+    "url": "/Hackernews-Clone/static/js/runtime-main.83df34fd.js"
+  }
+]);
